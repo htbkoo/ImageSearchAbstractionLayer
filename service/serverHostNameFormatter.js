e3868e90f42b7b5a5621fd078fc19253ba857a58
H.T. Koo <h1017760@connect.hku.hk>
2017-07-17 08:14:11 +0800

    removed unused obsolete files FreeCodeCamp -  API Backend project - Image Search Abstraction Layer

--- service/serverHostNameFormatter.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    appendProtocolToHostName: function (hostname, protocol) {
-        protocol = protocol || "https";
-        return protocol.toString().concat("://").concat(hostname);
-    }
-};
