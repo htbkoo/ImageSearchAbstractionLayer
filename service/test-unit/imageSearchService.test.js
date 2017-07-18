8116e33c81e8a9658ddbd928c25164b2e99afe5f
H.T. Koo <h1017760@connect.hku.hk>
2017-07-18 23:26:06 +0800

    fixed wrong implementation of sinon stubbing FreeCodeCamp -  API Backend project - Image Search Abstraction Layer

--- service/test-unit/imageSearchService.test.js
+++ service/test-unit/imageSearchService.test.js
@@ -10,21 +10,21 @@ var pixabayImageSearcher = require('../pixabayImageSearcher');
 describe("imageSearchService", function () {
     it("should return search results in searchAndPersist", sinon.test(function () {
         //    given
-        var stub_pixabayImageSearcher = sinon.stub(pixabayImageSearcher, 'search');
         var someResults = {
-            "url": "http://i3.kym-cdn.com/photos/images/newsfeed/000/024/741/lolcats-funny-picture-lalalalala.jpg",
-            "snippet": "Image - 24741] | LOLcats | Know Your Meme",
-            "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmiqU8mQTLyQ5aaOIuf4jglqdWUvdaPC3mhYrI54GS29Dq0UTBhzYTqLHe",
-            "context": "http://knowyourmeme.com/photos/24741-lolcats"
+            "keyA": "valueA",
+            "keyB": "valueB"
         };
+        var stub_pixabayImageSearcher = this.stub(pixabayImageSearcher, 'search');
         stub_pixabayImageSearcher.withArgs("someQuery").returns(Promise.resolve(someResults));
 
         //    when
         var promise = imageSearchService.searchAndPersist("someQuery");
 
         //    then
-        return promise.then(function(actualResults){
+        return promise.then(function (actualResults) {
             test.expect(actualResults).to.deep.equal(someResults);
-        }).catch()
+        }).catch(function (err) {
+            throw err;
+        });
     }));
 });
