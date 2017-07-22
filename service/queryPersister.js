f4f8bdc6bce07364c36d9ef409783c77bacc4bef
H.T. Koo <h1017760@connect.hku.hk>
2017-07-22 21:58:32 +0800

    implemented persist in queryPersister FreeCodeCamp -  API Backend project - Image Search Abstraction Layer

--- service/queryPersister.js
+++ service/queryPersister.js
@@ -2,8 +2,28 @@
  * Created by Hey on 20 Jul 2017
  */
 
-module.exports = {
-    "persist": function (query) {
+var COLLECTION_NAME_LATEST_QUERIES = 'queries';
 
+var moment = require('moment');
+
+var mongoDbConnectionManager = require('./mongoDbConnectionManager');
+// var connectToMongoDb = mongoDbConnectionManager.getOrReuseMongoDbConnection;
+
+module.exports = {
+    "persist": function (query, result) {
+        return mongoDbConnectionManager.getOrReuseMongoDbConnection()
+            .then(function (db) {
+                return db.collection(COLLECTION_NAME_LATEST_QUERIES);
+            })
+            .then(function (collection) {
+                return collection.insertOne({
+                    "query": query,
+                    "timestamp": moment().toISOString()
+                });
+            })
+            .then(function (insertResult) {
+                console.log(insertResult);
+                return result;
+            })
     }
 };
---
+++ service/test-unit/queryPersister.test.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Hey on 20 Jul 2017
+ */
+var test = require('chai');
+var format = require('string-format');
+var moment = require('moment');
+var sinon = require('../test-util/sinonTestHelper').sinon;
+
+var mockMongo = require('mongo-mock');
+mockMongo.max_delay = 0;
+var mockMongoClient = mockMongo.MongoClient;
+
+var queryPersister = require('../queryPersister');
+var mongoDbConnectionManager = require('../mongoDbConnectionManager');
+
+describe("queryPersister", function () {
+    "use strict";
+    var COLLECTION_NAME_LATEST_QUERIES = 'queries';
+
+    describe("persist", function () {
+        var mongoUrl = "someHost";
+        var stub = {};
+
+        afterEach(function () {
+            moment.now = function () {
+                return +new Date();
+            };
+            Object.keys(stub)
+                .forEach(function (key) {
+                    stub[key].restore();
+                });
+        });
+
+        it("should be able to persist query with creation timestamp", function () {
+            //    given
+            moment.now = function () {
+                return new Date(mockCurrentTime);
+            };
+            var aQuery = "query", aSearchResult = "results", mockCurrentTime = "2013-02-04T22:44:30.652Z";
+            var mockDbConnection = mockMongoClient.connect(mongoUrl);
+
+            stub.mongoDbConnectionManager_getOrReuseMongoDbConnection = sinon.stub(mongoDbConnectionManager, "getOrReuseMongoDbConnection");
+            stub.mongoDbConnectionManager_getOrReuseMongoDbConnection.returns(mockDbConnection);
+
+            //    when
+            var promise = queryPersister.persist(aQuery, aSearchResult);
+
+            //    then
+            var handlerForCleanUp = {};
+            return promise.then(function (returnValue) {
+                test.expect(returnValue).to.be.equal(aSearchResult);
+
+                return mockDbConnection;
+            }).then(function (db) {
+                handlerForCleanUp.db = db;
+
+                var collection = db.collection(COLLECTION_NAME_LATEST_QUERIES);
+                handlerForCleanUp.collection = collection;
+                return collection.findOne({
+                    "query": aQuery,
+                    "timestamp": mockCurrentTime
+                })
+            }).then(function (data) {
+                test.expect(data["query"]).to.equal(aQuery);
+            }).then(function () {
+                // truncate
+                handlerForCleanUp.collection.toJSON().documents.length = 0;
+                handlerForCleanUp.db.close();
+            }).catch(function (err) {
+                throw err;
+            });
+        });
+    });
+});
