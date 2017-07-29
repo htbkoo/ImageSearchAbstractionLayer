e1fac371e4e3e96a5225042bb59a738ace425216
H.T. Koo <h1017760@connect.hku.hk>
2017-07-29 15:57:18 +0800

    refactored queryPersister further FreeCodeCamp -  API Backend project - Image Search Abstraction Layer

--- service/queryPersister.js
+++ service/queryPersister.js
@@ -9,32 +9,37 @@ var moment = require('moment');
 
 var mongoDbConnectionManager = require('./mongoDbConnectionManager');
 
-function connectToMongoDb() {
-    return mongoDbConnectionManager.getOrReuseMongoDbConnection();
+function mongoDb() {
+    var connection = mongoDbConnectionManager.getOrReuseMongoDbConnection();
+    return {
+        "connectAndGetCollection": function (collectionName) {
+            return connection.then(function (db) {
+                return db.collection(collectionName);
+            })
+        }
+    };
 }
 
 module.exports = {
     "persist": function (query, result) {
-        return connectToMongoDb().then(function (db) {
-            return db.collection(COLLECTION_NAME.LATEST_QUERIES);
-        }).then(function (collection) {
-            return collection.insertOne({
-                "query": query,
-                "timestamp": moment().toDate()
-            });
-        }).then(function (insertResult) {
-            console.log(insertResult);
-            return result;
-        })
+        return mongoDb().connectAndGetCollection(COLLECTION_NAME.LATEST_QUERIES)
+            .then(function (collection) {
+                return collection.insertOne({
+                    "query": query,
+                    "timestamp": moment().toDate()
+                });
+            }).then(function (insertResult) {
+                console.log(insertResult);
+                return result;
+            })
     },
     "latest": function () {
-        return connectToMongoDb().then(function (db) {
-            return db.collection(COLLECTION_NAME.LATEST_QUERIES);
-        }).then(function (collection) {
-            return collection.find({}, {"_id": 0, "query": 1, "timestamp": 1})
-                .sort({"timestamp": -1})
-                .limit(LIMIT_NUM_LATEST_SEARCHES)
-                .toArray();
-        });
+        return mongoDb().connectAndGetCollection(COLLECTION_NAME.LATEST_QUERIES)
+            .then(function (collection) {
+                return collection.find({}, {"_id": 0, "query": 1, "timestamp": 1})
+                    .sort({"timestamp": -1})
+                    .limit(LIMIT_NUM_LATEST_SEARCHES)
+                    .toArray();
+            });
     }
 };
