1c4562e913ef0a517ad85a6f5e13a56f1f2ebe1f
H.T. Koo <h1017760@connect.hku.hk>
2017-08-03 19:16:15 +0800

    updated indexing ensuring mechanism FreeCodeCamp -  API Backend project - Image Search Abstraction Layer

--- service/mongoDbInitializer.js
+++ service/mongoDbInitializer.js
@@ -5,21 +5,25 @@
 var format = require('string-format');
 
 var mongoDbConnectionManager = require("./mongoDbConnectionManager");
-var existingIndexChecker = require("./existingIndexChecker");
 var expectedIndexRetriever = require("./expectedIndexRetriever");
 var EXPECTED_INDEXES = expectedIndexRetriever.retrieve();
 
 module.exports = {
     "createIndexesIfMissing": function () {
         var collection_queries;
-        return mongoDbConnectionManager.getOrReuseMongoDbConnection().then(function (db) {
-            return db.collection("queries");
-        }).then(function (collection) {
-            return Promise.all(
-                EXPECTED_INDEXES.map(function (index) {
-                    return collection.ensureIndex(index.key, index.op);
+        console.log("Creating indexes if missing");
+        return Promise.all(
+            Object.keys(EXPECTED_INDEXES)
+                .map(function (key) {
+                    console.log("for collection: " + key);
+                    return mongoDbConnectionManager.getOrReuseMongoDbConnection().then(function (db) {
+                        return db.collection(key);
+                    }).then(function (collection) {
+                        var index = EXPECTED_INDEXES[key];
+                        console.log("ensured index: " + JSON.stringify(index.key));
+                        return collection.ensureIndex(index.key, index.op);
+                    });
                 })
-            );
-        });
+        );
     }
 };
